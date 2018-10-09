95c0fc10bdf2ea00f38fab97fa2154f3f9164555
Viknesh Alagappan <alagapp1@msu.edu>
2018-10-08 20:28:38 -0400

    fixed bugs

--- pdftonotes/js/pdftolist.js
+++ pdftonotes/js/pdftolist.js
@@ -4,6 +4,7 @@ $(function () {
     var badWords=[];
     var pdfjsLib = window['pdfjs-dist/build/pdf'];
     var result_PDF;
+    
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
     $('input[type="file"]').change(function (e) {
         var fileName = (e.target.files[0])?e.target.files[0].name:"No file selected";
@@ -41,6 +42,9 @@ $(function () {
         }, 2000);
     });
     $('#btnConvert').click(function () {
+        
+        var headerDelim=($('#headerDelim').is(':checked'))?true:false;
+        var finalText_array=[];
         var finalText="";
         var fileReader = new FileReader();
         var pageStart=parseInt($('#pageStart').val());
@@ -59,39 +63,14 @@ $(function () {
                 if(isNaN(excludeEnd))excludeEnd=0;
                 if(isNaN(excludeStart))excludeStart=0;
                 var count=pageStart;
-                for(var i=pageStart;i<=pageEnd;i++){
-                    pdf.getPage(i).then(function (page) {
-                        // you can now use *page* here
-                        page.getTextContent().then(function(textContent){
-                            finalText+='\n'
-                            for(var j=excludeStart;j<textContent.items.length-excludeEnd;j++){
-                                var detectNumber=textContent.items[j].str.replace(/ /g,"");
-                                var parsedInt=parseInt(detectNumber);
-                                if($('#pageNumberDetection').is(':checked')&&!isNaN(parsedInt)&&detectNumber.length<3){
-                                    continue;
-                                }else{
-                                    var textItem=textContent.items[j].str;
-                                    var remove=false;
-                                    for(var k=0;k<badWords.length;k++){
-                                        if(textItem.indexOf(badWords[k])!=-1&&badWords[k].length>=textItem.length/2){
-                                            remove=true;
-                                            break;
-                                        }
-                                    }
-                                    if(remove){
-                                        continue;
-                                    }else{
-                                        finalText+=textItem;
-                                    }
-                                }
-
-                            }
-                            if(count==pageEnd){
-                                convertFromPDF(finalText);
-                            }
-                            count++;
-                            // console.log(finalText);
-                        })
+                
+                    for(var i=pageStart;i<=pageEnd;i++){
+                    getPageText(pdf,i,excludeStart,excludeEnd,function(result,index){
+                        finalText_array[index-1]=result;
+                        if(finalText_array.length===pdf.numPages&&finalText_array.every(element => element !== null)){
+                            console.log(finalText_array);
+                            convertFromPDF(finalText_array.join());
+                        }
                         
                     });
                 }
@@ -107,12 +86,67 @@ $(function () {
         // console.log(userText);
 
     });
+    function getPageText(pdf,i,excludeStart,excludeEnd,callback){
+        var finalText="";
+        
+        var addTab=false;
+        pdf.getPage(i).then(function (page) {
+            // you can now use *page* here
+            page.getTextContent().then(function(textContent){
+                
+                console.log(textContent);
+                if(!$('#addNewLine').is(':checked'))finalText+='\n';
+                for(var j=excludeStart;j<textContent.items.length-excludeEnd;j++){
+                    var detectNumber=textContent.items[j].str.replace(/ /g,"");
+                    var parsedInt=parseInt(detectNumber);
+                    if($('#pageNumberDetection').is(':checked')&&detectNumber.indexOf('.')==-1&&!isNaN(parsedInt)&&detectNumber.length<3){
+                        continue;
+                    }else{
+                        var textItem=textContent.items[j].str;
+                        var remove=false;
+                        for(var k=0;k<badWords.length;k++){
+                            if(textItem.indexOf(badWords[k])!=-1&&badWords[k].length>=textItem.length/2){
+                                remove=true;
+                                break;
+                            }
+                        }
+                        if(remove){
+                            continue;
+                        }else{
+                            if(textItem==' '){
+                                addTab=false;
+                            }
+                            if($('#addNewLine').is(':checked')&&(textItem.length>150||textContent.items[j].height>10)){
+                                finalText+='\n';
+                                addTab=true;
+                            }else if(headerDelim&&addTab&&textItem!=' '){
+                                finalText+='\t'
+                                addTab=false;
+                            }
+                            if(($('#splitter3').val()=='NUM'||$('#splitter2').val()=='NUM'||$('#splitter1').val()=='NUM')&&textItem.length>3){
+                                for (var index = 100; index > 0; index--) {
+                                    if (textItem.indexOf(index + $('#numDelim').val()) != -1)
+                                    textItem = textItem.replace(index + $('#numDelim').val(), index+"PLA.'CEHOLDER"+$('#numDelim').val());
+                                }
+                            }
+                            finalText+=textItem;
+                        }
+                    }
+
+                }
+                callback(finalText,i);
+                // console.log(finalText);
+            })
+            
+        });
+    }
     function convertFromPDF(text){
         result_PDF=convertText(text);
         $('#result').text(result_PDF);
     }
     function convertText(userText){
-        console.log(userText);
+        var headerDelim=($('#splitter1').val()=='BIG')?true:false;
+        // console.log(userText);
         var split1 = $('#splitter1').val();
         var split2 = $('#splitter2').val();
         var split3 = $('#splitter3').val();
@@ -167,6 +201,21 @@ $(function () {
                 userText = userText.replace(i + numDelim, "\n\t");
             }
         }
-        return userText;
+        var userTextArray=userText.split('\n');
+        var elementsToRemove=[];
+        for(var i=0;i<userTextArray.length;i++){
+            while(userTextArray[i].indexOf(' ')==0){
+                userTextArray[i]=userTextArray[i].replace(' ','');
+            }
+            if(userTextArray[i]==""){
+                elementsToRemove.push(i);
+            }
+        }
+        for(var i=elementsToRemove.length-1;i>=0;i--){
+            userTextArray.splice(elementsToRemove[i],1);
+        }
+        var userTextArray_joined=userTextArray.join('\n');
+        userTextArray_joined=userTextArray_joined.replace(/PLA.'CEHOLDER/g,'')
+        return userTextArray_joined;
     }
 })
